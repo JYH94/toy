0a0676d83d401f765636dda478ab7c93f0de7332
JYH <dydgusc66@naver.com>
2024-05-20 18:10:28 +0900

    좌표클릭 -> 위도경도 체크 Fishing.jsx [71줄]

--- toy_board_front/src/pages/selectProject/fishing/Fishing.jsx
+++ toy_board_front/src/pages/selectProject/fishing/Fishing.jsx
@@ -9,7 +9,7 @@ const loadScript = (url) => {
         script.src = url;
         script.onload = resolve;
         script.onerror = reject;
-        
+
         document.head.appendChild(script);
     });
 };
@@ -18,62 +18,98 @@ const loadScript = (url) => {
 const Fishing = () => {
 
     const apiKey = process.env.REACT_APP_KAKAOMAP_API_KEY;
-    const [point, setPoint] = useState();
+    const [point, setPoint] = useState({
+        La: '',
+        Ma: ''
+    });
 
 
     useEffect(() => {
         const loadKakaoMap = async () => {
             try {
                 await loadScript(`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&libraries=LIBRARY`);
+                let { kakao } = window;
                 // Kakao Maps SDK 스크립트가 로드된 후에 kakao.maps 객체가 사용 가능한지 확인
                 if (window.kakao && window.kakao.maps) {
-                    const { kakao } = window;
-                    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+                    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+                        mapOption = {
+                            center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+                            level: 1 // 지도의 확대 레벨
+                        };
+
+                    // 지도를 생성합니다    
+                    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+                    // 주소-좌표 변환 객체를 생성합니다
+                    var geocoder = new kakao.maps.services.Geocoder();
+
+                    var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
+                        infowindow = new kakao.maps.InfoWindow({ zindex: 1 }); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
 
 
-                    const container = document.getElementById('map');
-                    const options = {
-                        center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-                        level: 3
-                    };
-                    const map = new kakao.maps.Map(container, options);
+                    // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+                    searchAddrFromCoords(map.getCenter(), displayCenterInfo);
 
-                    // 장소 검색 객체를 생성합니다
-                    const ps = new kakao.maps.services.Places();
+                    // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+                    kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+                        searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
+                            if (status === kakao.maps.services.Status.OK) {
+                                var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+                                detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
 
-                    // 키워드로 장소를 검색합니다
-                    ps.keywordSearch('저수지', placesSearchCB);
+                                var content = '<div class="bAddr">' +
+                                    '<span class="title">법정동 주소정보</span>' +
+                                    detailAddr +
+                                    '</div>';
 
+                                // 마커를 클릭한 위치에 표시합니다 
+                                marker.setPosition(mouseEvent.latLng);
+                                marker.setMap(map);
+                                console.log(mouseEvent.latLng);
+                                setPoint((pre) => ({
+                                    ...pre,
+                                    La: mouseEvent.latLng.La,
+                                    Ma: mouseEvent.latLng.Ma
+                                }))
 
-                    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-                    function placesSearchCB(data, status, pagination) {
-                        if (status === kakao.maps.services.Status.OK) {
-                            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-                            // LatLngBounds 객체에 좌표를 추가합니다
-                            const bounds = new kakao.maps.LatLngBounds();
-                            for (let i = 0; i < data.length; i++) {
-                                displayMarker(data[i]);
-                                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                                // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+                                infowindow.setContent(content);
+                                infowindow.open(map, marker);
                             }
-                            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-                            map.setBounds(bounds);
-                        }
+                        });
+                    });
+
+                    // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+                    kakao.maps.event.addListener(map, 'idle', function () {
+                        searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+                    });
+
+                    function searchAddrFromCoords(coords, callback) {
+                        // 좌표로 행정동 주소 정보를 요청합니다
+                        geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
                     }
 
-                    // 지도에 마커를 표시하는 함수입니다
-                    function displayMarker(place) {
-                        // 마커를 생성하고 지도에 표시합니다
-                        const marker = new kakao.maps.Marker({
-                            map: map,
-                            position: new kakao.maps.LatLng(place.y, place.x)
-                        });
 
-                        // 마커에 클릭이벤트를 등록합니다
-                        kakao.maps.event.addListener(marker, 'click', function () {
-                            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-                            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-                            infowindow.open(map, marker);
-                        });
+                    function searchDetailAddrFromCoords(coords, callback) {
+                        // 좌표로 법정동 상세 주소 정보를 요청합니다
+                        geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+                    }
+
+                    // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+                    function displayCenterInfo(result, status) {
+                        if (status === kakao.maps.services.Status.OK) {
+                            var infoDiv = document.getElementById('centerAddr');
+
+                            if (infoDiv) {
+                                for (var i = 0; i < result.length; i++) {
+                                    // 행정동의 region_type 값은 'H' 이므로
+                                    if (result[i].region_type === 'H') {
+                                        infoDiv.innerHTML = result[i].address_name;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
                     }
                 } else {
                     console.error('Failed to load Kakao Map script: Kakao maps object is not available.');
@@ -82,6 +118,8 @@ const Fishing = () => {
                 console.error('Failed to load Kakao Map script:', error);
             }
         };
+
+
         loadKakaoMap();
     }, []);
 
